ea12ce781e3fdce67cce4fe535defc748ae59c17
Denis Oliveira <denisdev3loper@gmail.com>
2024-01-08 12:14:01 -0300

    uso do redux toolkit

---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Produto } from '../../App'
+
+type CarrinhoState = {
+  itens: Produto[]
+}
+
+const initialState: CarrinhoState = {
+  itens: []
+}
+
+const carrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    adicionar: (state, action: PayloadAction<Produto>) => {
+      const produtos = action.payload
+
+      if (state.itens.find((produtos) => produtos.id === produtos.id)) {
+        alert('item já adicionado')
+      } else {
+        state.itens.push(produtos)
+      }
+    }
+  }
+})
+
+export const { adicionar } = carrinhoSlice.actions
+export default carrinhoSlice.reducer
